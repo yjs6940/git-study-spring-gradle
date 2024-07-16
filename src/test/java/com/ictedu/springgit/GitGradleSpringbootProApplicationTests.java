e53ec966cac7cc46f43103ab0db7b47b59eb8dec
YeoJinSoo <yjs6940@naver.com>
2024-07-16 15:35:06 +0900

    Git 연습용 프로젝트 생성

---
+++ src/test/java/com/ictedu/springgit/GitGradleSpringbootProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ictedu.springgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitGradleSpringbootProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
